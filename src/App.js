1498e4b92b00924e2e98a50a4745a550cf34a7b2
Walter <walter.clark24@yahoo.com>
2023-01-21 02:51:06 -0500

    search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import SafariPage from './components/SafariPage'
+import Search from './components/Search'
 function App() {
   return (
     <div className="app">
       <SafariPage />
+      <Search/>
     </div>
   );
 }
