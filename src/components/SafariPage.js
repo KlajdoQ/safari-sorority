33987fb6409c60e29cbca52e101a86f83aaa25ec
Tatiana <Bowmantatiana2@gmail.com>
2023-01-23 13:48:07 -0800

    animal form cb func

--- src/components/SafariPage.js
+++ src/components/SafariPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Header from './Header/Header'
 import Search from './Search'
-import NewAnimalForm "./NewAnimalForm"
+import NewAnimalForm from  "./NewAnimalForm"
 import PostsList from './PostsList'
 
 export default function SafariPage() {
+
+  // ADDING NEW ANIMAL (FORM CB FUNCTION)
+  // const addNewAnimal = (newAnimal) => {
+  //   const newData = [...whatever-the-name-of-the-og-data-is, newAnimal]
+  //   whatever-the-name-of-the-setter-func-is(newData)
+  // }
+
   return (
     <div>
         <Header />
