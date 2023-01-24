8cc96aa6908daf8218099a6499fda2278a41f709
shawn <shawnsmith0024@gmail.com>
2023-01-23 20:45:22 -0500

    set up props for postsLists and posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-export default function Posts() {
+
+export default function Posts({animal}) {
+  
+  
   return (
-    <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"Animal Name"} />
-      <h4>{"Animal name"}</h4>
-      <p>description: {"Animal description"}</p>
+    <li className="posts">
+      <img src={animal.image} alt={animal.name} />
+      <h2>{animal.name}</h2>
+      <h4>{animal.type}</h4>
+      <p><i>{animal.description}</i> </p>
       <button className='Likes'>Like🐾</button>
     </li>
   )
