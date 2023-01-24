4346a42819c7f13c9ef8f6997144bc838e9cc541
shawn <shawnsmith0024@gmail.com>
2023-01-24 09:19:24 -0500

    added some css and functionality to comment section

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import Comments from "./Comments"
 
+
 export default function Posts({animal}) {
   const [likes, setLikes] = useState(0)
   const handleLikes = () => setLikes(prevCount => prevCount + 1)
   return (
     <li className="posts">
-      <img src={animal.image} alt={animal.name} />
-      <h2>{animal.name}</h2>
-      <h4>{animal.type}</h4>
-      <p><i>{animal.description}</i> </p>
+      <img className= "animal-image" src={animal.image} alt={animal.name} />
+      <h2 className='name'>{animal.name}</h2>
+      <h4 className='type'>{animal.type}</h4>
+      <p className='description'><i>{animal.description}</i> </p>
       <div><Comments/></div>
       <button className='Likes' onClick={handleLikes}>{likes}🐾</button>
       
